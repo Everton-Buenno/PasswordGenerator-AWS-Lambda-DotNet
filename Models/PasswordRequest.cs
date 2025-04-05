9fa3c134e474629420af8a23b5a3d8a4b44bd778
Everton <evertonbueno2014@gmail.com>
2025-04-04 21:19:15 -0300

    Remover valor padrão da propriedade Length
    A linha `public int Length { get; set; } = 12;` foi alterada para `public int Length { get; set; }`, removendo o valor padrão de `12` para a propriedade `Length` na classe `PasswordRequest`.

--- Models/PasswordRequest.cs
+++ Models/PasswordRequest.cs
@@ -8,7 +8,7 @@ namespace PasswordGeneratorFunction.Models
 {
     public class PasswordRequest
     {
-        public int Length { get; set; } = 12;
+        public int Length { get; set; }
         public bool IncludeUppercase { get; set; } = true;
         public bool IncludeLowercase { get; set; } = true;
         public bool IncludeNumbers { get; set; } = true;
